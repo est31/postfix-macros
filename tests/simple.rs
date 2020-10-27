62d07760b1dcfbed3cc726f2aae4c84d3659bf30
est31 <MTest31@outlook.com>
2020-10-27 08:46:18 +0100

    Improve tests a little

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,8 +1,19 @@
 use postfix_macros::postfix_macros;
 
-postfix_macros!(
-#[test]
-fn check_hi_equals_hi() {
-	"hi".assert_eq!("hi".to_string());
+postfix_macros! {
+	#[test]
+	fn hi_equals_hi() {
+		"hi".assert_eq!("hi".to_string());
+	}
 }
-);
+
+postfix_macros! {
+	#[test]
+	fn some_matches() {
+		let v = Some(42);
+		// Test that idents work
+		let b = v.matches!(Some(42));
+		// Test that Groups work
+		let bb = b && (None::<()>).matches!(None);
+	}
+}
