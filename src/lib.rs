8f2ad78e50464683e619c73184a8b69ad34a0b24
est31 <MTest31@outlook.com>
2020-10-28 18:32:31 +0100

    Add an unwrap_or macro

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,37 @@ val.iter()
 
 */
 pub use postfix_macros_impl::postfix_macros;
+
+/**
+Either unwraps the content passed to the macro,
+or executes the code block passed as second argument.
+
+The macro is very similar to functions like [Option::unwrap_or](std::Option::unwrap_or),
+Unlike functions like [Option::unwrap_or], he code block is lazily
+The macro is built for evaluation in postfix contexts,
+to facilitate quick
+
+```
+# use postfix_macros::{postfix_macros, unwrap_or};
+# postfix_macros! {
+let v = Err(());
+let mut w = 0;
+for i in 0..3 {
+	w += i;
+	v.unwrap_or!{ continue };
+	break
+}
+assert_eq!(w, 3);
+# }
+```
+*/
+#[macro_export]
+macro_rules! unwrap_or {
+	($v:expr, $($w:tt)*) => {
+		if let Some(inner) = $v.map(|v| Some(v)).unwrap_or(None) {
+			inner
+		} else {
+			$($w)*
+		}
+	};
+}
