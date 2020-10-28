65817dbdf2e0fbb0b8c7a09eccaa982ff01ee763
est31 <MTest31@outlook.com>
2020-10-28 11:37:04 +0100

    Start a file that tests expression endings
    This is some fairly tricky code. Better add tests for it.

---
+++ tests/expr_end.rs
@@ -0,0 +1,17 @@
+//! Tests for when the expression ends
+use postfix_macros::postfix_macros;
+
+macro_rules! stringify_eq {
+	($v:tt, $($w:tt)*) => {{
+		assert_eq!(stringify!($v), stringify!({$($w)*}));
+		$v
+	}};
+}
+
+postfix_macros! {
+	#[test]
+	fn mut_doesnt_end_expr() {
+		let _ = &mut ().stringify_eq!(&mut ());
+		let _ = 0 -(0).stringify_eq!((0));
+	}
+}
