39e254ceaefe8a0f560ea1509b87d3241689d75d
est31 <MTest31@outlook.com>
2020-11-03 15:50:05 +0100

    Add another failing testcase
    This time about prefix operator search before if/match/else etc.

--- tests/expr_end.rs
+++ tests/expr_end.rs
@@ -146,6 +146,23 @@ postfix_macros! {
 }
 
 /*
+// Test that prefix operator search is enabled for match/if/etc.
+postfix_macros! {
+	#[test]
+	fn ref_match_belongs() {
+		&match false { _ => "hi" }
+			.to_string()
+			.stringify_eq!(&match false { _ => "hi" }.to_string());
+	}
+	#[test]
+	fn multi_ref_match_belongs() {
+		&&&&&match false { _ => "hi" }
+			.to_string()
+			.stringify_eq!(&&&&&match false { _ => "hi" }.to_string());
+	}
+}*/
+
+/*
 // Test that things which belong into an if expr
 // but terminate a chained . expr work correcly.
 postfix_macros! {
